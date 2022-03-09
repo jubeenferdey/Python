4a76861df51819f8af4eb4ba0d0482fb9d3d1654
unknown <cat22@xcl.edu>
2022-03-09 16:19:14 +0530

    Added Digits.py

---
+++ Digits.py
@@ -0,0 +1,6 @@
+num = int(input("Enter the Number: "))
+words = {1:"one",3:"three",2:"two",4:"Four",5:"Five", 
+         6:"Six", 7:"Seven", 8:"Eight", 9:"Nine", 0:"Zero"
+        }
+digit = num%10
+print(digit)
