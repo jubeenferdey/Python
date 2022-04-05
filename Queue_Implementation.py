39a048be40194370238f6c818e7162914bcb47e2
Jubeen Ferdey <59162930+jubeenferdey@users.noreply.github.com>
2022-04-05 14:19:39 +0530

    Update Queue_Implementation.py
    Updated Queue with some descriptive print statements, for better understanding.

--- Queue_Implementation.py
+++ Queue_Implementation.py
@@ -1,9 +1,9 @@
 import numpy as np
-
 arr = np.array([1, 2, 3, 4, 5])
+print("The Array, before Dequeing: \t\t ", arr)
+print("---------------------------------------")
 #arr = np.delete(arr,0)
 for i in range(0,len(arr)):
-    print(arr[0])
+    print("The Element to be popped: ",arr[0])
     arr = np.delete(arr,0)
-    print(arr)
-    
+    print("Array after Dequeing the First Element: ",arr)
